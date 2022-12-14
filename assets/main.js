1ae792e849747a4a66f2813e82c225a9f383aeaf
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-14 14:23:25 +0100

    First scripts

--- assets/main.js
+++ assets/main.js
@@ -1,66 +1,80 @@
+// FOR TEST ONLY
 const TASKS = [{
     name : "Nom tache",
     date : "2022-12-25",
+    remainingTime: 200,
     description : "lorem ipsum dolor sic amet...",
     type : "1",
-    urgent : true,
 },{
     name : "Ab tache",
     date : "2022-12-25",
+    remainingTime: 2,
     description : "lorem ipsum dolor sic amet...",
     type : "0",
-    urgent : false,
 },{
     name : "K tache",
     date : "2022-12-25",
+    remainingTime : 180,
     description : "lorem ipsum dolor sic amet...",
     type : "0",
-    urgent : false,
 },{
     name : "Q tache",
     date : "2022-12-25",
+    remainingTime : 22,
     description : "lorem ipsum dolor sic amet...",
     type : "0",
-    urgent : false,
-}
-]
+}]
 
-console.log(TASKS[0].urgent);
-console.log(TASKS[1].urgent);
-
-const SORTEDALPHA = TASKS.sort((a,b) => a.name > b.name);
-console.log(SORTEDALPHA);
-const NOTDKW = TASKS.sort((a,b) => a.name < b.name);
-console.log(NOTDKW);
-
-function remainingTime(){
-let today = new Date().getTime();
-let fixed = new Date(TASKS[0].date).getTime();
-let remaining = fixed - today;
-
-let days = Math.floor(remaining /(1000 * 60 * 60 * 24));
-let hours = Math.floor(remaining % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-let min = Math.floor(remaining % (1000 * 60 * 60) / (1000 * 60));
-
-let timeRemaining = `${days}d${(hours > 0)? ` ${hours}h` : ""} ${(min > 0)? `${min}m ` : ""}left`
-return timeRemaining;
+// CLASS FOR OBJECT
+class NewTask {
+    constructor(name, description, date, type){
+        this.name = name,
+        this.description = description,
+        this.date = date,
+        this.delay = checkDelay(date),
+        this.remainingTime = remainingTime(this.delay),
+        this.type = type
+    }
 }
 
-remainingTime();
+let trying = new NewTask("jardinage", "2022-12-13", "manger du chocolat", "dormir");
+console.log(trying);
 
 // DisplayingTask
 
 // AddingTask
 
-
 // ChangingStatus
 
 // Sorting
+const SORTEDALPHA = TASKS.sort((a,b) => a.name > b.name);
+const SORTEDTIME = SORTEDALPHA.sort((a,b) => a.remainingTime - b.remainingTime);
 
 // LocalStorageSaving
+
+// Calculate milliseconds
+function checkDelay(dateGiven){
+    let today = new Date().getTime();
+    let fixed = new Date(dateGiven).getTime();
+    let remaining = fixed - today;
+
+    return remaining;
+}
 
 
 // CalculateRemainingTime
 
+function remainingTime(delay){
+
+    let days = Math.floor(delay /(1000 * 60 * 60 * 24));
+    let hours = Math.floor(delay % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let min = Math.floor(delay % (1000 * 60 * 60) / (1000 * 60));
+    
+    let timeRemaining = `${Math.abs(days)}d${(hours > 0)? ` ${Math.abs(hours)}h` : ""} ${(min > 0)? `${Math.abs(min)}m ` : ""}${(min > 0) ? "left" : "ago"}`
+    
+    return timeRemaining;
+    }
+    
+    remainingTime();
 
 // DisplayRemainingTime
